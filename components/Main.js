f38ee93f0b934f033003cfd3360636167f3e0418
Juliette Rapala <juliette@eventric.com>
2020-02-14 12:34:06 -0600

    Create palette

---
+++ components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Main = () => {
+	return (
+		<body>
+			<div className='palette'>
+				<div className='swatches'>
+					<div className='master-tour-red-500 swatch-title'>
+						<h3>master-tour-red</h3>
+					</div>
+					<span className='color-swatch master-tour-red-50'>master-tour-red-50</span>
+					<span className='color-swatch master-tour-red-100'>master-tour-red-100</span>
+					<span className='color-swatch master-tour-red-200'>master-tour-red-200</span>
+					<span className='color-swatch master-tour-red-300'>master-tour-red-300</span>
+					<span className='color-swatch master-tour-red-400'>master-tour-red-400</span>
+					<span className='color-swatch master-tour-red-500'>master-tour-red-500</span>
+					<span className='color-swatch master-tour-red-600'>master-tour-red-600</span>
+					<span className='color-swatch master-tour-red-700'>master-tour-red-700</span>
+					<span className='color-swatch master-tour-red-800'>master-tour-red-800</span>
+					<span className='color-swatch master-tour-red-900'>master-tour-red-900</span>
+				</div>
+			</div>
+		</body>
+	)
+}
+
+export default Main
