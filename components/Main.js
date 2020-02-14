07aa22e08f54a25394cafdaab9e49228e47d2862
Juliette Rapala <juliette@eventric.com>
2020-02-14 14:48:53 -0600

    Add sketch config file and map swatches

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Main = () => {
+	const range = [ 50 ]
+	for (let i = 100; i < 1000; i = i + 100) { range.push(i) }
+
 	return (
 		<body>
 			<div data-sketch-symbol="Palette/master-tour-red" className='palette'>
@@ -8,16 +11,11 @@ const Main = () => {
 					<div data-sketch-symbol="Palette/SwatchTitle" className='master-tour-red-500 swatch-title'>
 						<h3 data-sketch-text="SwatchTitleText">master-tour-red</h3>
 					</div>
-					<div data-sketch-symbol="Color/master-tour-red-50" className='color-swatch master-tour-red-50'>master-tour-red-50</div>
-					<div data-sketch-symbol="Color/master-tour-red-100" className='color-swatch master-tour-red-100'>master-tour-red-100</div>
-					<div data-sketch-symbol="Color/master-tour-red-200" className='color-swatch master-tour-red-200'>master-tour-red-200</div>
-					<div data-sketch-symbol="Color/master-tour-red-300" className='color-swatch master-tour-red-300'>master-tour-red-300</div>
-					<div data-sketch-symbol="Color/master-tour-red-400" className='color-swatch master-tour-red-400'>master-tour-red-400</div>
-					<div data-sketch-symbol="Color/master-tour-red-500" className='color-swatch master-tour-red-500'>master-tour-red-500</div>
-					<div data-sketch-symbol="Color/master-tour-red-600" className='color-swatch master-tour-red-600'>master-tour-red-600</div>
-					<div data-sketch-symbol="Color/master-tour-red-700" className='color-swatch master-tour-red-700'>master-tour-red-700</div>
-					<div data-sketch-symbol="Color/master-tour-red-800" className='color-swatch master-tour-red-800'>master-tour-red-800</div>
-					<div data-sketch-symbol="Color/master-tour-red-900" className='color-swatch master-tour-red-900'>master-tour-red-900</div>
+					{range.map(num => (
+						<div data-sketch-symbol={`Color/master-tour-red-${num}`} className={`color-swatch master-tour-red-${num}`}>
+							{`master-tour-red-${num}`}
+						</div>
+					))}
 				</div>
 			</div>
 		</body>
---
+++ html-sketchapp.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	file: '/out/index.html',
+	outDir: 'dist',
+	viewports: {
+	  Desktop: '1024x768',
+	},
+  };
